fd38280cec11ed6d620e268afdae87c3a20b1821
raghammer1 <raghagarwal@gmail.com>
2024-09-01 16:40:11 +1000

    sc

--- backend/auth/postRegister.js
+++ backend/auth/postRegister.js
@@ -40,7 +40,7 @@ const postRegister = async (req, res) => {
     res.cookie('token', token, {
       secure: true, // Set to true in production with HTTPS
       maxAge: 24 * 60 * 60 * 1000, // Cookie expires in 24 hours
-      SameSite: 'none',
+      sameSite: 'none',
     });
 
     // Respond with the user's data upon successful registration
